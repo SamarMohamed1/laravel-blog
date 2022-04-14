69fbb8adb652f7c878b8ba1cf4e94a371b3ab3cf
SamarMohamed1 <samar.mohamed.hassan@gmail.com>
2022-04-14 20:38:08 +0200

    make controllers,models and requests

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserController extends Controller
+{
+    public function signup(RegisterRequest $request){
+
+        $input = $request->all();
+
+        if(User::where('email',$input['email'])->exists()){
+            return response()->json(["this email already has account"],403);
+        }else{
+
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        return $this->success([
+            'token' => $user->createToken('tokens')->plainTextToken
+        ]);
+    }
+
+
+
+
+
+    // $attr = $request->validate([
+    //     'name' => 'required|string|max:255',
+    //     'email' => 'required|string|email|unique:users,email',
+    //     'password' => 'required|string|min:6|confirmed'
+    // ]);
+
+    // $user = User::create([
+    //     'name' => $attr['name'],
+    //     'password' => bcrypt($attr['password']),
+    //     'email' => $attr['email']
+    // ]);
+
+    // return $this->success([
+    //     'token' => $user->createToken('tokens')->plainTextToken
+    // ]);
+    }
+
+    public function login(LoginRequest $request){
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+
+            $user = Auth::user();
+            return $this->success([
+                'token' => $user->createToken('tokens')->plainTextToken
+            ]);
+
+
+        }else{
+            return response()->json("invalid email or password" , 403);
+        }
+
+
+
+        // $attr = $request->validate([
+        //     'email' => 'required|string|email|',
+        //     'password' => 'required|string|min:6'
+        // ]);
+
+        // if (!Auth::attempt($attr)) {
+        //     return $this->error('Credentials not match', 401);
+        // }
+
+        // return $this->success([
+        //     'token' => auth()->user()->createToken('API Token')->plainTextToken
+        // ]);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +23,40 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//route to signup
+Route::post('/signup',[UserController::class,'signup']);
+
+//route to login
+Route::post('/login',[UserController::class,'login']);
+
+//route to get the user posts
+Route::get('/user-posts', [PostController::class ,'userPosts'])->middleware('auth:sanctum');
+
+//route to get all posts
+Route::get('/posts',[PostController::class,'allPosts'])->middleware('auth:sanctum');
+
+//route to get update post
+Route::get('/update-posts/{post}', [PostController::class ,'update'])->middleware('auth:sanctum');
+
+//route to add new post
+Route::post('/Add-posts', [PostController::class ,'store'])->middleware('auth:sanctum');
+
+
+// Route::post('/sanctum/token', function (Request $request) {
+//     $request->validate([
+//     'email' => 'required|email',
+//     'password' => 'required',
+//     'device_name' => 'required',
+//     ]);
+
+//     $user = User::where('email', $request->email)->first();
+
+//     if (! $user || !Hash::check($request->password, $user->password)) {
+//         throw ValidationException::withMessages([
+//             'email' => ['The provided credentials are incorrect.'],
+//             ]);
+//             }
+
+//             return $user->createToken($request->device_name)->plainTextToken;
+//            });
